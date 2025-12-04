4283e2596b7107e6f838975f6c7f2cc72a031c05
tony-airis <inv4sv@yandex.ru>
2025-12-04 13:10:17 +0300

    добавил стены для лабиринта

--- MazeField.h
+++ MazeField.h
@@ -12,18 +12,48 @@ public:
     Player player;
 
     void CreateField() {
-        walls.push_back(Wall(0, 0, 20, 1440));
-        walls.push_back(Wall(0, 0, 1440, 100));
-        walls.push_back(Wall(90, 90, 91, 100));
-        walls.push_back(Wall(90, 90, 230, 100));
-        walls.push_back(Wall(210, 90, 230, 100));
-        //walls.push_back(Wall(0, 41, 250, 40));
-        //walls.push_back(Wall(0, 41, 250, 40));
-        //walls.push_back(Wall(0, 41, 250, 40));
-        //walls.push_back(Wall(0, 41, 250, 40));
-        //walls.push_back(Wall(0, 41, 250, 40));
-        //walls.push_back(Wall(0, 41, 250, 40));
-        //walls.push_back(Wall(0, 41, 250, 40));
+    /*walls.push_back(Wall(0, 0, 550, 10));
+    walls.push_back(Wall(0, 0, 10, 180));
+    walls.push_back(Wall(0, 180, 100, 10));
+    walls.push_back(Wall(90, 90, 100, 10));
+    walls.push_back(Wall(170, 90, 10, 180));
+    walls.push_back(Wall(0, 260, 180, 10));
+    walls.push_back(Wall(0, 260, 10, 550-260));
+    walls.push_back(Wall(0, 540, 360, 10));
+    walls.push_back(Wall(360, 360, 10, 190));
+    walls.push_back(Wall(360, 360, 100, 10));
+    walls.push_back(Wall(450, 180, 10, 190));
+    walls.push_back(Wall(540, 0, 10, 550));
+    walls.push_back(Wall(450, 450, 10, 100));
+    walls.push_back(Wall(450, 540, 100, 10));
+    walls.push_back(Wall(360, 90, 180, 10));
+    walls.push_back(Wall(360, 90, 10, 100));
+    walls.push_back(Wall(260, 0, 10, 370));
+    walls.push_back(Wall(260, 260, 100, 10));
+    walls.push_back(Wall(90, 360, 180, 10));
+    walls.push_back(Wall(90, 360, 10, 100));
+    walls.push_back(Wall(90, 460, 180, 10));*/
+    walls.push_back(Wall(0, 0, 550, 10));
+    walls.push_back(Wall(0, 0, 10, 180));
+    walls.push_back(Wall(0, 170, 90, 10));
+    walls.push_back(Wall(90, 90, 90, 10));
+    walls.push_back(Wall(170, 90, 10, 180));
+    walls.push_back(Wall(0, 260, 180, 10));
+    walls.push_back(Wall(0, 260, 10, 550 - 260));
+    walls.push_back(Wall(0, 540, 360, 10));
+    walls.push_back(Wall(360, 360, 10, 190));
+    walls.push_back(Wall(360, 360, 100, 10));
+    walls.push_back(Wall(450, 180, 10, 190));
+    walls.push_back(Wall(540, 0, 10, 550));
+    walls.push_back(Wall(450, 450, 10, 90));
+    walls.push_back(Wall(450, 540, 90, 10));
+    walls.push_back(Wall(360, 90, 180, 10));
+    walls.push_back(Wall(360, 90, 10, 90));
+    walls.push_back(Wall(260, 0, 10, 370));
+    walls.push_back(Wall(260, 260, 100, 10));
+    walls.push_back(Wall(90, 360, 180, 10));
+    walls.push_back(Wall(90, 360, 10, 100));
+    walls.push_back(Wall(90, 460, 180, 10));
     }
     
     void MayMove(string command) {
